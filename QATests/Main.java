0ffde2ea52d38aa1e96e652ee6cb73357aae6e42
ChenShahaf <cshahaf@gmail.com>
2023-02-01 05:18:13 +0200

    last Version

--- QATests/Main.java
+++ QATests/Main.java
@@ -1,16 +1,16 @@
 package QATests;
 
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Main {
 
@@ -22,47 +22,33 @@ public class Main {
 		
 
 	}
-	public static WebDriver openDrive() {
+
+	public static void serachJava(){
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\chen\\Desktop\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		return driver;
-		
-	}
-	public static void serachJava(){
-		WebDriver driver = openDrive();
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\chen\\Desktop\\chromedriver_win32\\chromedriver.exe");
 		driver.get("https://www.google.com/");
-		driver.findElement(By.name("q")).sendKeys("Java");
 		
-		 WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(2));
-			
-
-			WebElement list=driver.findElement(By.className("sbtc"));
-			
-			
-			List<WebElement> rows=list.findElements(By.tagName("li"));
-
-			
-			for(WebElement elem:rows) {
-			System.out.println(elem.getText());
-			}
+		WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(40));
 		
+		String cssOfInputField = "input[name='q']";
+		
+		WebElement inputFieldQ = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(cssOfInputField)));
+		
+		inputFieldQ.sendKeys("java");
+		
+		//WebDriverWait wait2= new WebDriverWait(driver,Duration.ofSeconds(2000));
+		
+		String cssOfSearchield = "input[name='btnK']";
+		
+		WebElement searchBtn =  wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(cssOfSearchield)));
+		
+		searchBtn.click();
+		
+	driver.close();
+			
+	driver.quit();
+			
 
-	}
-	
-	//VALIDATION FUNCTION
-	
-	public static void checkOpenChtomeCorrectly() {
-		WebDriver testDriver = openDrive();
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\chen\\Desktop\\chromedriver_win32\\chromedriver.exe");
-		testDriver.get("https://www.google.com/");
-		String check = testDriver.getCurrentUrl();
-		if(check.equals("https://www.google.com/")) {
-			System.out.println("Test case - passed ok");
-		}
-		else {
-			System.out.println("Test case - Failed");
-		}
 	}
 	
 
